57582457d057d0ef27b23450c21823e430f7bf6d
keero <47709146+keero1@users.noreply.github.com>
2023-02-12 00:57:47 +0800

    removed document.write

--- js/main.js
+++ js/main.js
@@ -3,23 +3,25 @@ $('.btn').on('click', function () {
     $this.button('loading');
     setTimeout(function () {
         const urlArray = [
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
             'https://www.youtube.com/watch?v=UIp6_0kct_U',
-            'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+            'https://www.youtube.com/watch?v=UIp6_0kct_U'
         ]
         $this.button('reset');
         var randomNum = Math.floor(Math.random() * urlArray.length);
-        document.write("<h2>Thank You!</h2>");
         window.location.href = urlArray[randomNum];
     }, 1000);
 });
 
 setTimeout(function () {
     const urlArray = [
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
         'https://www.youtube.com/watch?v=UIp6_0kct_U',
-        'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        'https://www.youtube.com/watch?v=UIp6_0kct_U'
     ]
 
     var randomNumber = Math.floor(Math.random() * urlArray.length);
-    document.write("<h2>Thank You!</h2>");
     window.location.href = urlArray[randomNumber];
 }, 5000);
